0c2891efda9b7dd38c321cb7a6649542dd389bf8
HARINARAYAN KUMAR <harinarayankumar806@gmail.com>
2025-02-28 09:07:49 +0530

    Update App.js
    Signed-off-by: HARINARAYAN KUMAR <harinarayankumar806@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Warning from "./pages/Warning";
+import FakeCrash from "./pages/FakeCrash";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/warning" element={<Warning />} />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/warning" element={<Warning />} />
+        <Route path="/fake-crash" element={<FakeCrash />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
