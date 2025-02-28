eb35215d5bc1e72c87f8878670537e115f287a7f
HARINARAYAN KUMAR <harinarayankumar806@gmail.com>
2025-02-28 08:48:35 +0530

    Update FakeCrash.js
    Signed-off-by: HARINARAYAN KUMAR <harinarayankumar806@gmail.com>

--- src/pages/FakeCrash.js
+++ src/pages/FakeCrash.js
@@ -1,10 +1,20 @@
 import React from "react";
 
 function FakeCrash() {
+  const playAudio = () => {
+    const audio = new Audio('path/to/audio/file.mp3');
+    audio.play().catch(error => {
+      console.error('Error playing audio:', error);
+    });
+  };
+
   return (
     <div style={{ background: "black", color: "white", textAlign: "center", padding: "50px" }}>
       <h1>💻 SYSTEM FAILURE!</h1>
       <p>Windows has encountered a critical error and will restart.</p>
+      <button id="playButton" onClick={playAudio} style={{ marginTop: "20px", padding: "10px 20px" }}>
+        Play Sound
+      </button>
     </div>
   );
 }
